8f76962706d048106db689cb1f606f0aa81daa33
paiwandbahman <96263192+paiwandbahman@users.noreply.github.com>
2022-07-03 17:38:40 +0300

    second

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ if (process.env.NODE_ENV === "production") {
   console.log("production");
   app.use(express.static(path.join(__dirname, "frontend/build")));
 
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 } else {
